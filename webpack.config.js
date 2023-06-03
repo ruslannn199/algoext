b76332be24a96c5fd6e99f765b596473c14f1394
ruslannn199 <ruslannn199@gmail.com>
2023-06-03 21:11:55 +0300

    init: start project
    implement base architecture, set configs

---
+++ webpack.config.js
@@ -0,0 +1,136 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const postcssPresetEnv = require('postcss-preset-env');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+const isDev = process.env.NODE_ENV === 'development';
+const mode = process.env.NODE_ENV === 'development' ? 'development' : 'production';
+const target = isDev ? 'web' : 'browserslist';
+
+const filename = (ext) => (isDev ? `[name].${ext}` : `[contenthash].${ext}`);
+
+module.exports = {
+  mode: mode,
+  target: target,
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    }
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+
+  entry: {
+    index: './src/index',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: filename('js'),
+    assetModuleFilename: 'images/[contenthash][ext]',
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.ico/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]'
+        }
+      },
+      {
+        test: /\.svg$/i,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[contenthash][ext]',
+        },
+      },
+      {
+        test: /\.(wav|ogg|mp3)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'sound/[contenthash][ext]',
+        }
+      },
+      {
+        test: /\.(s[ac]|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { 
+              publicPath: '',
+              esModule: isDev,
+            },
+          },
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  postcssPresetEnv({ stage: 0 }),
+                ]
+              }
+            }
+          },
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: filename('css'),
+    }),
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      minify: !isDev,
+    }),
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, 'src/manifest.json'),
+					to: path.resolve(__dirname, 'dist/manifest.json')
+				}
+			]
+		}),
+  ],
+
+  devtool: isDev ? 'source-map' : 'hidden-nosources-source-map',
+  devServer: {
+    port: 9000,
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    open: true,
+    hot: isDev,
+    compress: true,
+    historyApiFallback: true,
+  },
+}
